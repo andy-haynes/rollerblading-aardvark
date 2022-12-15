a6669ec217e3894be256fb86f187dd5ffbff43e0
Andy Haynes <andy@near.org>
2022-12-14 16:48:04 -0800

    basic controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,86 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+interface AuthSignature {
+  accountId: string;
+  blockHeight: number;
+  publicKey: string;
+  signedBlockHeight: string;
+}
+
+interface Profile {
+  age?: number;
+  education?: string;
+  favoriteJohnCarpenterMovie?: string;
+  gender?: boolean;
+  income?: number;
+  interests?: number;
+  location?: string;
+  occupation?: string;
+}
+
+interface CreateQueryRequest {
+  attributes: Array<{ key: string; value: string }>;
+  authSignature: AuthSignature;
+  totalAccounts: number;
+}
+
+interface ViewResultsRequest {
+  authSignature: AuthSignature;
+  organizationId: string;
+  queryId: string;
+}
+
+interface RegisterConsumerRequest {
+  name: string;
+  contractId: string;
+  publicKey: string;
+  signedTx: string;
+}
+
+interface RegisterProducerRequest {
+  accountId: string;
+  authSignature: AuthSignature;
+  profile: Profile;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('consumer/:id')
+  getConsumer(@Param('organizationId') organizationId: string) {
+    return {
+      organizationId,
+    };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('consumer')
+  registerConsumer(@Body() consumer: RegisterConsumerRequest) {
+    return {
+      organizationId: `${consumer.name}-123`,
+      transactionId: '4yRSZ11rrEVGYwoE99LFN9VKPtU3Mz9owr8JxwtJ9mjN',
+      balance: 100,
+    };
+  }
+
+  @Post('consumer/:organizationId/query')
+  createQuery(@Body() query: CreateQueryRequest) {
+    return {
+      estimatedCost: Math.random() * 10000,
+      queryId: Math.floor(Math.random() * 10000 * query.totalAccounts),
+    };
+  }
+
+  @Post('consumer/:organizationId/results/:queryId')
+  viewResults(@Body() viewResults: ViewResultsRequest) {
+    return {
+      cost: Math.random() * 10000,
+      results: [],
+    };
+  }
+
+  @Post('producer')
+  registerProducer(@Body() producer: RegisterProducerRequest) {
+    return {
+      accountId: producer.accountId,
+    };
   }
 }
